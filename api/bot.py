c78c1f3e399457e3e9bfe1853a1a668117da202f
Michael Kaufmann <mkaufmann421@gmail.com>
2024-03-09 18:47:42 -0500

    Add potential plan of attack to api/bot.py

---
+++ api/bot.py
@@ -0,0 +1,28 @@
+from rit_api import *
+
+# TODO:
+# Determine when to start program, or how to wait until game starts/repeat for multiple rounds
+# Determine whether we can read who makes what positions, as using this information could be quite telling, especially for bluffs
+# Determine optimal parameters: trade sizing, edge, fade, slack
+
+# Game loop:
+# Get start of game information (case, trader, limits)
+# Set starter positions, e.g. traps for market orders that sweep the entire book
+# Loop each tick:
+#   Get current order book and portfolio
+#   Determine fair price based on supply and demand (and our fair price based on our current position? fade factor in blog)
+#   Determine target portfolio based on current portfolio
+#   Set initial spread positions around fair price based on target portfolio
+#       - This should be roughly even around fair price when our position is net zero
+#         If we are long, we want to sell more aggressively, and vice versa
+#         In LT1, (the first game) we get long and short positions of 5000 added to our current position,
+#         so we will need to rebalance whenever this occurs. (fade should cover this)
+#       - We want to "penny" out our competitors when rational. This means that we should 
+#         develop a fudge factor (slack in blog) that we can tune that allows our spread to narrow or widen.
+#   Set additional spreads at different increments within the order book to profit from partial order book sweeps (scales in blog)
+#   Potentially place bluff orders into the order book, but at safe levels
+#   Modify starter positions in the case we were 'penny'd out. 
+#   Send and cancel orders
+#       
+
+
